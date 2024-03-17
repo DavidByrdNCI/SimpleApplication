714eae5cd791200d5a9ad4c1a459753ba21a8694
daviddbyrd <151666012+daviddbyrd@users.noreply.github.com>
2024-03-17 19:59:53 +0000

    added HTTPS

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,10 @@ sudo npm install -g pm2
 # stop any instance of our application running currently
 pm2 stop simple_application
 # change directory into folder where application is downloaded
-cd SimpleApplication
+cd SimpleApplication/
 # Install application dependencies 
 npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
 # Start the application with the process name simple_application using pm2
-pm2 start ./bin/www --name simple_application
+pm2 start ./bin/www --name SimpleApplication --env production
